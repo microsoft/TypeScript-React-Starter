c6b108e5a17e36fa14c1ef8fce74824d8ac14f10
Alan R. Soares <alanrsoars@gmail.com>
2017-05-26 13:10:57 +1200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
+import { enthusiasm } from './reducers/index';
+import { StoreState } from './types/index';
+
+const mockStore = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={mockStore}>
+      <App />
+    </Provider>, div);
 });

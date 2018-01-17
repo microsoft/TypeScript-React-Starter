d100d0276ecc1020674275ecf01f1032558b301c
Patrick Steele-idem <steeleid@amazon.com>
2018-01-16 17:30:49 -0700

    Fixes #101 - Cannot find name 'Cheerio'
    Upgraded dependencies and added prettier
    
    Because of prettier, all stylistic rules have been removed from tslint.json. App was previously broken because of linting errors.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
--- src/containers/Hello.tsx
+++ src/containers/Hello.tsx
@@ -1,19 +1,21 @@
-import Hello from '../components/Hello';
-import * as actions from '../actions/';
-import { StoreState } from '../types/index';
-import { connect, Dispatch } from 'react-redux';
+import Hello from "../components/Hello";
+import * as actions from "../actions/";
+import { StoreState } from "../types/index";
+import { connect, Dispatch } from "react-redux";
 
 export function mapStateToProps({ enthusiasmLevel, languageName }: StoreState) {
   return {
     enthusiasmLevel,
-    name: languageName,
+    name: languageName
   };
 }
 
-export function mapDispatchToProps(dispatch: Dispatch<actions.EnthusiasmAction>) {
+export function mapDispatchToProps(
+  dispatch: Dispatch<actions.EnthusiasmAction>
+) {
   return {
     onIncrement: () => dispatch(actions.incrementEnthusiasm()),
-    onDecrement: () => dispatch(actions.decrementEnthusiasm()),
+    onDecrement: () => dispatch(actions.decrementEnthusiasm())
   };
 }
 

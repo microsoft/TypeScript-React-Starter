3d065c34d41adb433d3d8dec284ee04ea32e2a19
Afzal Sayed <afzalsayed96@gmail.com>
2018-10-03 11:01:13 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,11 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { enthusiasm } from './reducers/index';
 import { StoreState } from './types/index';
+import { EnthusiasmAction } from './actions/index';
 
 import './index.css';
 
-const store = createStore<StoreState>(enthusiasm, {
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
   enthusiasmLevel: 1,
   languageName: 'TypeScript',
 });

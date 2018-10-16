6d07f578f8132e9a27bd58cdaee3beb7fa0f08cb
Saad Hassan <hassan.saad.mail@gmail.com>
2018-10-16 20:47:35 +0530

    Implementation of Redux Middleware

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,17 @@ import * as ReactDOM from 'react-dom';
 
 import Hello from './containers/Hello';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { enthusiasm } from './reducers/index';
 import { StoreState } from './types/index';
-
+import logger from './middlewares/logger'
 import './index.css';
 
 const store = createStore<StoreState>(enthusiasm, {
   enthusiasmLevel: 1,
   languageName: 'TypeScript',
-});
+},
+applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
